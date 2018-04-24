9d1829f2ab09366db2ec9f05e66a31c17f400168
Budi.Hartanto <muhammadbudi.h@gmail.com>
2018-04-24 08:46:03 +0700

    Initial commit

---
+++ app/src/main/java/com/budi/test/usingfirebase/MainActivity.java
@@ -0,0 +1,13 @@
+package com.budi.test.usingfirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
